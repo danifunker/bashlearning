80039982a6ab493182283f5ac36f72b79b362af9
Dani Sarfati <dani@funkervogt.com>
2016-08-04 16:58:32 -0400

    Fixed $ in var messages

--- clearlog-withvar.sh
+++ clearlog-withvar.sh
@@ -2,7 +2,7 @@
 #./excersize1.sh
 #Backs up messages file and deletes it afterwards
 LOGFILE=!~/messages
-cp LOGFILE LOGFILE.old
+cp $LOGFILE $LOGFILE.old
 #echo "" > messages
-cat /dev/null > LOGFILE
+cat /dev/null > $LOGFILE
 exit 1
